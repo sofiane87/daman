3a44307e2ee56dbcb5e1ab342595753c71ae5b1a
sofiane87 <sofiane-mahiou@hotmail.fr>
2021-03-05 20:45:39 +0000

    Intialise repository

---
+++ __init__.py
@@ -0,0 +1,88 @@
+from typing import Union
+from pathlib import Path
+from logging import getLogger
+from configparser import ConfigParser
+
+from data_manager.configure.aws import configure_aws
+
+
+logger = getLogger(__name__)
+HOME_DIR = Path().home()
+CONFIG_DIR = HOME_DIR / ".data_manager"
+
+CLOUD_SERVICES = {"aws": configure_aws}
+
+
+def configure(
+    storage_name: str,
+    service: str,
+    local_dir: Union[Path, str] = None,
+    allocated_space: int = None,
+    service_settings: dict = None,
+) -> None:
+    """Short summary.
+
+    Parameters
+    ----------
+    storage_name : str
+        Description of parameter `storage_name`.
+    service : str
+        Description of parameter `service`.
+    local_dir : Union[Path, str]
+        Description of parameter `local_dir`.
+    allocated_space : int
+        Description of parameter `allocated_space`.
+    service_settings : dict
+        Description of parameter `service_settings`.
+
+    Returns
+    -------
+    None
+        Description of returned object.
+
+    """
+
+    dm_config = ConfigParser()
+    # storing service config
+    dm_config.add_section("service")
+    dm_config["service"] = {"service": service, "name": storage_name}
+    # storing local config
+    dm_config.add_section("local")
+    if local_dir is None:
+        local_dir = str((CONFIG_DIR / "data").resolve())
+
+    dm_config["local"] = {
+        "data_dir": local_dir,
+        "registery": str(CONFIG_DIR / "registery.json"),
+        "allocated_space": allocated_space,
+    }
+
+    if service_settings is not None:
+        configure_service(service=service, settings=service_settings)
+
+
+def configure_service(service: str, settings: dict) -> None:
+    """Short summary.
+
+    Parameters
+    ----------
+    service : str
+        Description of parameter `service`.
+    settings : dict
+        Description of parameter `settings`.
+
+    Returns
+    -------
+    None
+        Description of returned object.
+
+    """
+    service = service.lower()
+
+    # check service is supported
+    assert_msg = f"'{service}' is not currently supported. Available services: {list(CLOUD_SERVICES.keys())}"
+    assert service in CLOUD_SERVICES, assert_msg
+
+    # call config function
+    config_func = CLOUD_SERVICES[service]
+    config_func(**settings)
---
+++ aws.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+from logging import getLogger
+from configparser import ConfigParser
+
+
+logger = getLogger(__name__)
+
+HOME = Path().home()
+AWS_DIR = HOME / ".aws"
+
+
+def configure_aws(
+    access_key_id: str, secret_access_key: str, region: str = None
+) -> None:
+    """Short summary.
+
+    Parameters
+    ----------
+    access_key_id : str
+        Description of parameter `access_key_id`.
+    secret_access_key : str
+        Description of parameter `secret_access_key`.
+    region : str
+        Description of parameter `region`.
+
+    Returns
+    -------
+    None
+        Description of returned object.
+
+    """
+    AWS_DIR.mkdir(parents=True, exist_ok=True)
+    logger.info(f"updating AWS credentials: {AWS_DIR / 'credentials'}")
+
+    aws_credentials = ConfigParser()
+    if (AWS_DIR / "credentials").exists():
+        with (AWS_DIR / "credentials").open("r") as fr:
+            aws_credentials.read_file(fr)
+
+    for key, value in [
+        ("aws_access_key_id", access_key_id),
+        ("aws_secret_access_key", secret_access_key),
+    ]:
+        set_value(aws_credentials, key, value)
+
+    with (AWS_DIR / "credentials").open("w") as fw:
+        aws_credentials.write(fw)
+
+    if region is not None:
+        logger.info(f"updating AWS config: {AWS_DIR / 'config'}")
+        aws_config = ConfigParser()
+        if (AWS_DIR / "config").exists():
+            with (AWS_DIR / "config").open() as fr:
+                aws_config.read_file(fr)
+
+        set_value(aws_config, "region", region)
+
+        with (AWS_DIR / "region").open("w") as fw:
+            aws_config.write(fw)
+
+
+def set_value(config, key, value):
+    """Short summary.
+
+    Parameters
+    ----------
+    config : type
+        Description of parameter `config`.
+    key : type
+        Description of parameter `key`.
+    value : type
+        Description of parameter `value`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    if not config.has_section("default"):
+        config.add_section("default")
+
+    if config.has_option("default", value):
+        if value != config[f"default_{key}", value]:
+            logger.warning(
+                f"'{key}' already defined and will be changed from '{config[f'default.{key}']}' to '{value}'"
+            )
+        else:
+            logger.info(f"'{key}' set to '{value}'")
+
+    config.set("default", key, value)
