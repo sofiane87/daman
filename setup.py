8924e9b4af380c28a341cd1a80c119b80ac3ad79
sofiane87 <sofiane-mahiou@hotmail.fr>
2021-03-18 17:47:55 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,16 +4,22 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fr:
+    requirements = fr.read().splitlines()
+
 setup(
     name="daman",
     author="Sofiane Mahiou",
     author_email="mahiou.sofiane@gmail.com",
-    description="A simple package that allows for the direct use of data stored and managed on a cloud storage service",
+    description=(
+        "A simple package that allows for the direct use of data"
+        " stored and managed on a cloud storage service"
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sofiane87/daman",
     python_requires=">=3.6.0",
-    version="0.1",
+    version="0.1.2",
     packages=find_packages("."),
     package_dir={"": "."},
     include_package_data=True,
@@ -33,4 +39,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=requirements,
 )
