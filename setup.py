ae37f55a4c008e440f95510c4122035187a4af00
sofiane87 <sofiane-mahiou@hotmail.fr>
2021-03-18 16:54:06 +0100

    Add pip files

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="daman",
+    author="Sofiane Mahiou",
+    author_email="mahiou.sofiane@gmail.com",
+    description="A simple package that allows for the direct use of data stored and managed on a cloud storage service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sofiane87/daman",
     python_requires=">=3.6.0",
     version="0.1",
     packages=find_packages("."),
@@ -17,4 +27,10 @@ setup(
             "dm_clear = daman.commands:clear_command",
         ]
     },
+    project_urls={"Bug Tracker": "https://github.com/sofiane87/daman/issues",},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
