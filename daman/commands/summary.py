e57fa00d7422cd658dba5137c6fa247a3d7f8576
sofiane87 <sofiane-mahiou@hotmail.fr>
2021-03-21 18:40:27 +0100

    Remove useless code

--- daman/commands/summary.py
+++ daman/commands/summary.py
@@ -3,7 +3,4 @@ from daman import daman as dm
 
 
 def summary_command():
-    max_rows = pd.get_option("display.max_rows")
-    pd.set_option("display.max_rows", None)
     print(dm.summary)
-    pd.set_option("display.max_rows", max_rows)
